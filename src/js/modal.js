dec76a5f77db19ac05331107f135435d3275b206
olegkovrigadev <olegkovrigadev@gmail.com>
2023-07-12 16:58:38 +0300

    Add modal button

--- src/js/modal.js
+++ src/js/modal.js
@@ -3,12 +3,16 @@
     // openModalBtn: document.querySelector("[data-modal-open]"),
     firstModalBtn: document.querySelector('[data-first-open]'),
     secondModalBtn: document.querySelector('[data-second-open]'),
+    thirdModalBtn: document.querySelector('[data-third-open]'),
+    fourthModalBtn: document.querySelector('[data-fourth-open]'),
     closeModalBtn: document.querySelector('[data-modal-close]'),
     modal: document.querySelector('[data-modal]'),
   };
   refs.closeModalBtn.addEventListener('click', toggleModal);
   refs.firstModalBtn.addEventListener('click', toggleModal);
   refs.secondModalBtn.addEventListener('click', toggleModal);
+  refs.thirdModalBtn.addEventListener('click', toggleModal);
+  refs.fourthModalBtn.addEventListener('click', toggleModal);
   function toggleModal() {
     refs.modal.classList.toggle('is-hidden');
   }
