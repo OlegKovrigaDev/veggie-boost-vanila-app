6fe40a5b419ac7543d74a9cedcd49ecafc6fc2a4
olegkovrigadev <olegkovrigadev@gmail.com>
2023-07-12 16:47:49 +0300

    Update modal

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,14 +1,15 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector("[data-modal-open]"),
-    closeModalBtn: document.querySelector("[data-modal-close]"),
-    modal: document.querySelector("[data-modal]"),
+    // openModalBtn: document.querySelector("[data-modal-open]"),
+    firstModalBtn: document.querySelector('[data-first-open]'),
+    secondModalBtn: document.querySelector('[data-second-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
   };
-
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
-
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs.firstModalBtn.addEventListener('click', toggleModal);
+  refs.secondModalBtn.addEventListener('click', toggleModal);
   function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
+    refs.modal.classList.toggle('is-hidden');
   }
 })();
